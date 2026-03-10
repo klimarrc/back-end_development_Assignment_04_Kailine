52fed052163fe9370e36f1ae21e05ff2c6fb4839
Klima2 <klima2@rrc.ca>
2026-03-10 13:03:03 -0500

    upadate: Update admincontroller

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -3,7 +3,6 @@ import { getFirestore, Firestore } from "firebase-admin/firestore";
 import { auth } from "firebase-admin/lib/auth/auth-namespace";
 import serviceAccount from "../authentication-m4-firebase-adminsdk-fbsvc-ae729b0456.json";
 
-const serviceAccount = require("../firebase-key.json") as ServiceAccount;
 
 initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
