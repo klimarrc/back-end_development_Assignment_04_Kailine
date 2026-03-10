131edf9e550acbdf20f1f32fe5db39bcebd9bbd9
Klima2 <klima2@rrc.ca>
2026-03-10 11:31:52 -0500

    feat: Create moldes folder and responseMolde.ts and update it.

---
+++ src/api/v1/models/respondeModel.ts
@@ -0,0 +1,17 @@
+
+export interface ApiResponse<T> {
+    status: string;
+    data?: T;
+    message?: string;
+    error?: string;
+    code?: string;
+}
+
+export const successResponse = <T>(
+    data?: T,
+    message?: string
+): ApiResponse<T> => ({
+    status: "success",
+    data,
+    message,
+});
